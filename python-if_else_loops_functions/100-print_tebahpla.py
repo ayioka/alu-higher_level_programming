72a3de12a5ec09edfa724a829fffe1cfac0ccbff
ayioka <s.ayioka@alustudent.com>
2024-10-23 09:06:01 -0700

    done

--- python-if_else_loops_functions/100-print_tebahpla.py
+++ python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-for i in range(25, -1, -1):
-    print(chr(i + 97) if i % 2 == 0 else chr(i + 65), end="")
+for i in range(26):
+    print("{:s}".format(chr(122 - i) if i % 2 == 0 else chr(90 - i)), end="")
 
