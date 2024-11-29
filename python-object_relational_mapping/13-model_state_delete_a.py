8247915629425cef40ef746c36a85e5e32c19c6e
ayioka <s.ayioka@alustudent.com>
2024-11-29 14:16:07 +0000

    done

--- python-object_relational_mapping/13-model_state_delete_a.py
+++ python-object_relational_mapping/13-model_state_delete_a.py
@@ -1,25 +1,36 @@
 #!/usr/bin/python3
-# script that deletes all 'State' objects with a name,
-# containing the letter 'a' from the database 'hbtn_0e_6_usa'
 """
-   import 'sys','sqlalchemy' & 'State'
+Deletes all State objects with a name containing the letter 'a'
+from the database hbtn_0e_6_usa.
 """
-import sqlalchemy
+
+import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from sys import argv
 from model_state import Base, State
 
-
 if __name__ == "__main__":
-    eng = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(argv[1],
-                                                                    argv[2],
-                                                                    argv[3]))
-    Base.metadata.create_all(eng)
-    Session = sessionmaker(bind=eng)
+    # Retrieve MySQL username, password, and database name from command-line arguments
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    # Create a database connection using SQLAlchemy
+    engine = create_engine(f"mysql+mysqldb://{username}:{password}@localhost:3306/{database}", pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+
+    # Create a session
+    Session = sessionmaker(bind=engine)
     session = Session()
-    states = session.query(State).filter(State.name.like('%a%'))
-    for state in states:
+
+    # Query and delete State objects containing the letter 'a' in their name
+    states_to_delete = session.query(State).filter(State.name.like('%a%')).all()
+    for state in states_to_delete:
         session.delete(state)
+
+    # Commit the changes to the database
     session.commit()
+
+    # Close the session
     session.close()
+
